84433553c92fc120ef6f89211f83cc8b6bd8520f
Yutao Wang <yutao.wang@avepoint.com>
2018-12-24 10:58:33 +0800

    Initial commit

---
+++ server.js
@@ -0,0 +1,51 @@
+const express = require('express')
+const hbs = require('hbs')
+const fs = require('fs')
+
+const app = express()
+
+hbs.registerPartials(__dirname + '/views/partials')
+app.set('view engine', hbs)
+app.use((req, res, next) => {
+  var now = new Date().toString('yyyy-MM-dd HH:mm:ss')
+  const log = `${now}: ${req.method} ${req.url}`
+  console.log(log)
+  fs.appendFile('server.log', log + '\n', err => {
+    if (err) {
+      console.log('Unable to append to server.log')
+    }
+  })
+  next()
+})
+// app.use((req, res, next) => {
+//   res.render('maintenance.hbs')
+// })
+app.use(express.static(__dirname + '/public'))
+
+
+hbs.registerHelper('getCurrentYear', () => new Date().getFullYear())
+hbs.registerHelper('screamIt', text => text.toUpperCase())
+
+app.get('/', (req, res) => {
+  // res.send('<h1>Hello World</h1>')
+  res.render('home.hbs', {
+    pageTitle: 'Home Page',
+    welcomeMessage: 'Welcome to my website'
+  })
+})
+
+app.get('/about', (req, res) => {
+  res.render('about.hbs', {
+    pageTitle: 'About Page'
+  })
+})
+
+app.get('/bad', (req, res) => {
+  res.send({
+    errorMessage: 'Page not found'
+  })
+})
+
+app.listen(3000, () => {
+  console.log('Server is set up in 3000')
+})

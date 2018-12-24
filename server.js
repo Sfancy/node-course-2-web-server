04bff3c6c12d3e9fa6af43da440e2c92b210ccd9
Yutao Wang <yutao.wang@avepoint.com>
2018-12-24 11:48:24 +0800

    Setup start script and heroku port

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const hbs = require('hbs')
 const fs = require('fs')
 
+const port = process.env.PORT || 3000
 const app = express()
 
 hbs.registerPartials(__dirname + '/views/partials')
@@ -21,7 +22,6 @@ app.use((req, res, next) => {
 //   res.render('maintenance.hbs')
 // })
 app.use(express.static(__dirname + '/public'))
-
 
 hbs.registerHelper('getCurrentYear', () => new Date().getFullYear())
 hbs.registerHelper('screamIt', text => text.toUpperCase())
@@ -46,6 +46,6 @@ app.get('/bad', (req, res) => {
   })
 })
 
-app.listen(3000, () => {
-  console.log('Server is set up in 3000')
+app.listen(port, () => {
+  console.log(`Server is set up in ${port}`)
 })
